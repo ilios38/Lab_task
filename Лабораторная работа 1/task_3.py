ca837c4b08f4cf590952659b1b1575ad548a8c12
ilios38 <136789903+ilios38@users.noreply.github.com>
2023-12-11 22:20:07 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+first_team = list_players[0:3]
+second_team = list_players[3:6]
+print(first_team, second_team, sep = '\n')
